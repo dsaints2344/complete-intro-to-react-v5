f29d9dd98c54f3de95d5f8e34d853f3840982659
dsaints 23444 <ddelossantosmateo@gmail.com>
2019-09-27 17:46:00 -0400

    Finishing course

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from "react";
+import React, {useState} from "react";
 import ReactDOM from "react-dom";
 import { Router, Link } from "@reach/router";
 import Details from "./Details";
 import SearchParams from "./SearchParams";
+import ThemeContext from "./ThemeContext"
 
 const App = () => {
+  const themeHook = useState('darkblue');
   return (
-    <div>
+    <ThemeContext.Provider value={themeHook}>
+      <div>
       <header>
         <Link to="/">Adopt Me!</Link>
       </header>
@@ -16,6 +19,8 @@ const App = () => {
         <Details path="/details/:id" />
       </Router>
     </div>
+    </ThemeContext.Provider>
+    
   );
 };
 
